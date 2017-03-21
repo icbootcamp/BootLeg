27919049b02a04635c847f808f13d7d5e8879c24
Lewis Jones <Lewis Jones@DESKTOP-GCM8O7P>
2017-03-22 10:57:17 +1300

    Create supplier list.

---
+++ BootLeg/Controllers/SupplierController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootLeg.Controllers
+{
+    public class SupplierController : Controller
+    {
+        // GET: Supplier
+        
+        public ActionResult SupplierName()
+            
+        {
+            BootLegEntities db = new BootLegEntities();
+
+            var supplierName = db.SupplierTypes.ToList();
+
+            return View(supplierName);
+        }
+
+        public ActionResult NewSupplier()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BootLeg/Models/SupplierClass1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BootLeg.Models
+{
+    public class SupplierClass1
+    {
+        public List<Supplier> Suppliers { get; set; }
+        public List<SupplierOrder> SupplierOrders { get; set; }
+        public List<Product> Product { get; set; }
+        public List<SupplierType> SupplierType { get; set; }
+
+    }
+}
